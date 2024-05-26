f64de6437702da0d93ded2739992ba935692190b
karwanjiru <168063764+karwanjiru@users.noreply.github.com>
2024-05-26 18:27:38 +0300

    trial

--- src/components/footer/footer.jsx
+++
@@ -1,9 +0,0 @@
-import './footer.css'
-
-export default function Footer(){
-    return(
-        <footer className="mt-5 bg-transparent bottom-0 right-0 left-0 w-full flex justify-between p-5 items-center text-lg border-t border-gray-800">
-            <div>Four 3</div>
-            <div><small>&#169;</small> 2024 All Rights Reserved</div>
-        </footer>
-    )}
--- src/pages/ChatBot.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function ChatBot() {
-    return (
-        <div>
-            <h1>ChatBot</h1>
-        </div> // added closing tag
-    );
-}
